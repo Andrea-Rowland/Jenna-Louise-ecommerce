1f8c7af715d48c99bea836b3ea992bc17c71a4e7
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-08-22 11:19:49 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Jenna-Louise-ecommerce.Art', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
